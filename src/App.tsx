d74be24f635fa2d1606e4e69ba2b9c9033bfd5b0
KilariSrikanth <kilarisrikanthchowdary@gmail.com>
2025-04-08 21:36:51 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+//import React from 'react'
+import NavBar from './components/NavBar'
+import Home from './components/Home'
+import About from './components/About'
+import Choose from './components/Choose'
+import Add from './components/Add'
+import Menu from './components/Menu'
+import BookTable from './components/BookTable'
+export default function App() {
+  return (
+    <div>
+
+      <NavBar />
+      <Home />
+      <About />
+      <Choose />
+      <Add />
+      <Menu />
+      <BookTable />
+
+
+    
+    </div>
+  )
+}
